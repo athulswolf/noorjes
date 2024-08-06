0803d51e7a8a1413e7c037037763425423a8e147
akhilashoklm10 <162559411+akhilashoklm10@users.noreply.github.com>
2024-08-06 16:44:28 +0400

    changed sub heading

--- src/components/Hero3.js
+++ src/components/Hero3.js
@@ -10,7 +10,7 @@ const Hero3 = () => {
       <div className='flex flex-col w-full h-full items-center gap-6 md:gap-16'>
         <div className='flex flex-col md:max-w-5xl text-center gap-2 md:gap-4'>
           <p className={`${styles.heroHeadText} capitalize`}>Introducing<span className='text-[#3AB7B3]'> NoorJes</span> - your favorite fragrance!</p>
-          <p className={`${styles.subHeading}`}>Discover the fragrance that perfectly aligns with your distinctive style.Your journey to a perfect refreshing day begins here with our perfume!</p>
+          <p className={`${styles.subHeading}`}>Explore the best perfume in Dubai that perfectly complements your unique style. Your journey to a refreshing day begins here with our exclusive Dubai perfume brands. Buy best perfume online UAE.</p>
         </div>
         <img src={video} alt="phone" className='w-full md:w-full' />
       </div>
