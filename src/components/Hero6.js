abfd5db76e9d73b0c94907b97036cf08b125e7de
akhilashoklm10 <162559411+akhilashoklm10@users.noreply.github.com>
2024-08-06 17:34:17 +0400

    Added Alt text
    changed content

--- src/components/Hero6.js
+++ src/components/Hero6.js
@@ -9,12 +9,12 @@ const Hero6 = () => {
     return (
         <div className='relative flex flex-col md:flex-row w-full px-6 md:px-36 py-6 md:py-10 bg-[#F5FBFB] overflow-hidden gap-x-10' id='about'>
             <div className='flex md:flex-1 h-full justify-center items-center my-auto'>
-                <img src={Order} alt='family' className='py-6 w-full md:w-11/12' />
+                <img src={Order} alt='Online Perfume Store UAE' className='py-6 w-full md:w-11/12' />
             </div>
             <div className='flex md:flex-1 h-full flex-col my-auto items-center md:items-start gap-4'>
                 <div className='md:max-w-lg flex flex-col gap-2 md:gap-4 text-left'>
                     <p className={`${styles.heroHeadText}`}>Why choose us?</p>
-                    <p className={`${styles.subHeading}`}>Elevate your senses with NoorJes Imagine Perfume. Immerse yourself in our world where premium quality, eco-conscious choices, and globally inspired scents redefine the fragrance experience like never before.</p>
+                    <p className={`${styles.subHeading}`}>Elevate your senses with NoorJes Imagine Perfume, a top Dubai perfume brand. Discover how our commitment to premium quality, eco-conscious choices, and globally inspired scents offers a new level of perfume in Dubai. Buy best perfume online UAE.</p>
                     <div className='flex flex-row gap-2'>
                         <FaCircleCheck className='w-6 h-6 text-[#3AB7B3]' />
                         <p className={`${styles.subHeading}`}>Unique Blends: Artfully curated, setting us apart.</p>
