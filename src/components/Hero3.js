8bf0bd9b3bf4939d63e82ef016e81aeea01281fa
akhilashoklm10 <162559411+akhilashoklm10@users.noreply.github.com>
2024-08-10 16:35:57 +0400

    Update Hero3.js

--- src/components/Hero3.js
+++ src/components/Hero3.js
@@ -10,7 +10,7 @@ const Hero3 = () => {
       <div className='flex flex-col w-full h-full items-center gap-6 md:gap-16'>
         <div className='flex flex-col md:max-w-5xl text-center gap-2 md:gap-4'>
           <p className={`${styles.heroHeadText} capitalize`}>Introducing<span className='text-[#3AB7B3]'> NoorJes</span> - your favorite fragrance!</p>
-          <p className={`${styles.subHeading}`}>Explore the Best perfume for women in Dubai that perfectly complements your unique style. Your journey to a refreshing day begins here with our exclusive Dubai perfume brands. Buy best perfume online UAE.</p>
+          <p className={`${styles.subHeading}`}>Explore the Best Eau de perfum in Dubai that perfectly complements your unique style. Your journey to a refreshing day begins here with our exclusive Dubai perfume brands. Buy best perfume online UAE.</p>
         </div>
         <img src={video} alt="Perfume online UAE" className='w-full md:w-full' />
       </div>
